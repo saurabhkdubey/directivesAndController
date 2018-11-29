d1b12a5983ac93446636ab1427faf9c3a6684833
saurabhkdubey <saurabh.dubey@hirepro.in>
2018-11-29 15:20:08 +0530

    remove console

--- src/app/main/controls.directive.js
+++ src/app/main/controls.directive.js
@@ -22,7 +22,6 @@
 
   function ControlsDirectiveCtrl($scope) {
       var vm = this;
-      console.log(!$scope.tasks);
       vm.clickFunction= clickFunction;
       function clickFunction(item){
           console.log('hello');
